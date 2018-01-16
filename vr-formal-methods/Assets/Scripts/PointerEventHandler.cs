1c6caaf417328c80dc48bc832598445b62891dbf
Iverson <ilp_1995@hotmail.com>
2018-01-15 23:24:09 -0200

    first version

---
+++ vr-formal-methods/Assets/Scripts/PointerEventHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerEventHandler : MonoBehaviour {
+
+	public void PointerEnterHandler()
+    {
+        Debug.Log("Entrou!");
+    }
+}
